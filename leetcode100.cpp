ea36aeb65e67cb01a8aac98a92f4b45189bd16ac
Ankita sahu <ankitasahu83964@gmail.com>
2026-07-17 15:49:44 +0530

    Implement binary number generator using queue
    This program generates and prints the first 'n' binary numbers using a queue.

---
+++ leetcode100.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+void generateBinary(int n) {
+    queue<string> q;
+    q.push("1");
+
+    while (n--) {
+        string s = q.front();
+        q.pop();
+
+        cout << s << " ";
+
+        q.push(s + "0");
+        q.push(s + "1");
+    }
+}
+
+int main() {
+    int n = 10;
+    generateBinary(n);
+    return 0;
+}
