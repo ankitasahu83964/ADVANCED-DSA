a64ddccb39dbdce1d5f73d3e9955e98c18664586
Ankita sahu <ankitasahu83964@gmail.com>
2026-08-28 00:16:23 +0530

    Add simple C++ program to find the largest number

---
+++ simple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 25, b = 15;
+
+    if (a > b)
+        cout << "Largest = " << a;
+    else
+        cout << "Largest = " << b;
+
+    return 0;
+}
