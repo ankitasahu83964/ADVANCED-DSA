a1bcf5ad8629447fbe47ae5c3d749e03c742a42c
Ankita sahu <ankitasahu83964@gmail.com>
2026-08-31 09:01:28 +0530

    Create leetcode144.cpp

---
+++ leetcode144.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solve(int index, vector<int>& candidates, int target,
+               vector<int>& current, vector<vector<int>>& result) {
+        
+        if (target == 0) {
+            result.push_back(current);
+            return;
+        }
+
+        if (index == candidates.size() || target < 0) {
+            return;
+        }
+
+        // Choose current candidate (can be used again)
+        if (candidates[index] <= target) {
+            current.push_back(candidates[index]);
+            solve(index, candidates, target - candidates[index], current, result);
+            current.pop_back();
+        }
+
+        // Skip current candidate
+        solve(index + 1, candidates, target, current, result);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+
+        solve(0, candidates, target, current, result);
+
+        return result;
+    }
+};
