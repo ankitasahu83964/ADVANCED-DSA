b1849fbcf6fc83d017c019f1929faffa61e82b4a
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-07 00:08:22 +0530

    five.cpp

---
+++ five.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int  a= 2.0/5;
+    cout<<a<<endl;
+
+    cout<<2.0/5<<endl;
+
+    float  b= 2.0/5;
+    cout<<b<<endl;
+
+    cout<<2.0/5<<endl;
+
+    double  c= 2.0/5;
+    cout<<c<<endl;
+
+    cout<<2.0/5<<endl;
+}
