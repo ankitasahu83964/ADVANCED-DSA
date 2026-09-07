4c3eb519c07497d01ae1ac01337ec5f72108ff93
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-07 23:40:00 +0530

    pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n=5; 
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=1){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }    
+
+    
+
+
+}
