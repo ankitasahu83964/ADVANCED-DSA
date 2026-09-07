53666403e2a2e470ed0091f8fc7723f0e65c872b
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-07 14:24:26 +0530

    seven.cpp

---
+++ seven.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=2;
+    int b=1;
+    if(a>b){
+        cout<<"a is greater"<<endl;
+    }
+    else
+       cout<<"b is greater"<<endl;
+}
