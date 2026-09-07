41686fca024673eda271f915065574ba5cc5faf9
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-07 14:23:49 +0530

    eight.cpp

---
+++ eight.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=6;
+    if(a>0){
+        cout<<" a is positive"<<endl;
+    }
+    else{
+        cout<<"a is negative"<<endl;
+    }
+}
