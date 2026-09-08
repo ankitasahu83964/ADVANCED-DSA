5c3b01e41c9da7e7b3bd850731dd9881757a4605
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-08 19:44:08 +0530

    pattern7.cpp

---
+++ pattern15.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=3;
+    int num=1;
+    for(int i=0;i<n;i++){  //outer loop
+        //char ch='A';
+        for(int j=0;j<n;j++){   //inner loop->start line
+            //cout<<ch;  
+            //ch=ch+1;
+            cout<<num;
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
