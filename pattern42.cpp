a5269fd9f851eae5599f23cbd9f9ce1e5684ca5d
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-09 11:56:34 +0530

    Add pattern42

---
+++ pattern42.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=4;
+    int i = 1;
+
+    while(i <= n) {
+
+        int space=i-1;
+        while(space){
+            cout << " ";
+            space--;
+
+        }
+        int j=1;
+        while (j<=i){
+            cout<<"*";
+            j++;
+
+        }
+        cout << endl;
+        i++;
+    }  
+    
+}    
