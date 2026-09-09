58e35ef50701ac67138bb1948520a6cd3b13f71a
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-10 00:37:24 +0530

    Add operator8

---
+++ operator8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=5;
+    cout<<"enter the value of n"<<endl;
+    for(int a=0,b=1; a>=0 && b>=1;a--,b--){
+        cout<<a<<" "<<b <<endl;
+    }
+
+}
+    
+    //cin>>n;
