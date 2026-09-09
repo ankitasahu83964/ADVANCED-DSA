fcffeaf343c96a0ad5158a961148052d28f3e9a3
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-10 00:36:19 +0530

    Add operator3

---
+++ operator3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //int a=4;
+    //int b=6;
+    int i=7;
+    cout<<(++i)<<endl;
+    cout<<(i++)<<endl;
+    cout<<(i--)<<endl;
+    cout<<(--i)<<endl;
+}
