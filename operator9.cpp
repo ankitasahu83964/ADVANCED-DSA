e260bb2f24a210bf6b6990e9a69ef5999df2a74c
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-10 00:37:34 +0530

    Add operator9

---
+++ operator9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=5;
+    cout<<"enter the value of n"<<endl;
+    for(int a=0,b=1,c=2; a>=0 && b>=1 && c>=2 ;a--,b--,c--){
+        cout<< a <<" "<< b <<" "<< c <<endl;
+    }
+
+}
+    
+    //cin>>n;
