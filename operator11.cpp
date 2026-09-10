dc0c6673681c3296087cedd03fa4e083ce678a96
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-10 19:16:54 +0530

    Add operator11

---
+++ operator11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=10;
+    cout<<"enter the value of n"<<endl;
+    //int a=0;
+    //int b=1;
+    int sum=0;
+    for(int i=1; i<=n; i++){
+        sum=sum+i;
+        
+    }
+    cout<<sum<<endl;
+    
+
+}
+    
