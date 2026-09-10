6b9954bf02eaaf7860bde21df0c12dbfe881c630
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-10 19:19:22 +0530

    Add operator21

---
+++ operator21.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=3;
+    cout<<a<<endl;
+
+    if(true){
+        int b=5;
+        cout<<b<<endl;
+    }
+    int b=1;
+    cout<<b<<endl;
+
+    for(int i=0;i<8;i++){
+        cout<<"HI"<<endl;
+    }
+    
+}
+    
