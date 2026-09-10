7a8821f539585b55faffd8e3beb559d58abc3865
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-10 19:17:39 +0530

    Add operator13

---
+++ operator13.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=0; i<=5; i++){
+        cout<<"hi"<<endl;
+        cout<<"hello"<<endl;
+        continue;
+
+        cout<<"reply toh karde"<<endl;
+
+    }
+
+}
+    
