316ba2daa63674e41c49c454441851e124b9f185
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-17 22:59:51 +0530

    DSA27.cpp

---
+++ DSA27.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch='a';
+    //int num=1;
+
+    cout<<endl;
+    switch(ch){
+        case 1: cout<<"first "<<endl;
+                cout<<"first again"<<endl;
+                break;
+        case '1': cout<<"character one"<<endl;
+                //break;
+        default: cout<<"It is default case"<<endl;               
+    }
+    cout<<endl;
+    
+    return 0;
+}
