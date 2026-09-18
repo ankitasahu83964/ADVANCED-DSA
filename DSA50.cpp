d3e739b7ad914f23274b70bfeedee1344da15008
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-18 22:13:18 +0530

    DSA50.cpp

---
+++ DSA50.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void printCounting(int n){
+    for(int i=1;i<=n;i++){
+        cout<<i<<endl;
+    }
+}
+int main(){
+    int n=5;
+    printCounting(n);
+    return 0;
+
+
+}
