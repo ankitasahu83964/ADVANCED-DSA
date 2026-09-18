e5618bdc154dcc392821a3cb710b0bd340feba25
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-18 22:11:36 +0530

    DSA43.cpp

---
+++ DSA43.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int power(){
+    int a=2;
+    int b=2;
+    int ans=1;
+    for(int i=1;i<=b;i++){
+        ans=ans*a;
+    }
+    return ans;    
+}
+int main(){
+    /*int a=2;
+    int b=4;
+    int answer=power(a,b);  
+    cout<< " answer is " <<answer << endl;
+    */
+    int ans=power();
+    cout<< " answer is " <<ans<< endl;
+    return 0;
+}
