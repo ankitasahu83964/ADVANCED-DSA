84e916448da3766a07af6977d6a1ae6fc4c0f74b
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-20 18:31:55 +0530

    DSA62.cpp

---
+++ DSA62.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //declare
+    int number[15];
+    //accessing an array
+    cout<<" value at 14 index "<<number[14]<<endl;
+
+    //cout<<" value at 20 index "<<number[20]<<endl;
+    //initializing an array
+    int second[3]={5,7,11};
+
+    //accessing an element
+    cout<<" value at 2 index "<<number[2]<<endl;
+
+    int third[15]={2,7};
+    int n=15;
+    cout<<"printing the array"<<endl;
+    //print the array
+    for(int i=0;i<n;i++){
+        cout<<third[i]<<" ";
+    }
+    //initialzing all location with 0
+    int fourth[10]={0};
+    n=10;
+    cout<<"printing the array"<<endl;
+    //print the array
+    for(int i=0;i<n;i++){
+        cout<<fourth[i]<<" ";
+    }
+
+    //initialzing all location with 1[not possible wiith below line]
+    int fifth[10]={1};
+    n=10;
+    cout<<"printing the array"<<endl;
+    //print the array
+    for(int i=0;i<n;i++){
+        cout<<fifth[i]<<" ";
+    }
+
+    cout<<endl<<"everything is fine"<<endl<<endl;
+    return 0;
+
+}
