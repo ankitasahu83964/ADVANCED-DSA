d13fdeb205ec3006cc858cbb61234af4dc4c2710
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-20 18:31:32 +0530

    DSA60.cpp

---
+++ DSA60.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //declare
+    int number[15];
+    //accessing an array
+    cout<<" value at 14 index "<<number[14]<<endl;
+
+    //cout<<" value at 20 index "<<number[20]<<endl;
+    //initializing an array
+    int second[3]={5,7,11};
+
+    //accessing an element
+    cout<<" value at 2 index "<<number[2]<<endl;
+
+    cout<<endl<<"everything is fine"<<endl<<endl;
+    return 0;
+
+}
