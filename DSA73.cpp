712592f8752bed70fa25602664a74f8063a3da0d
Ankita sahu <ankitasahu83964@gmail.com>
2026-09-22 13:08:51 +0530

    Add program to calculate square of a number

---
+++ DSA73.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int square(int n)
+{
+    return n * n;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    cout << "Square = " << square(n);
+
+    return 0;
+}
