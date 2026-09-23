62b4763949ec4c15ff68e357d1e1ace0a6702138
Ankita sahu <ankitasahu83964@gmail.com>
2026-09-23 12:44:51 +0530

    Create DSA74.cpp

---
+++ DSA74.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int largest(int a, int b, int c)
+{
+    if(a > b && a > c)
+        return a;
+    else if(b > c)
+        return b;
+    else
+        return c;
+}
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << "Largest = " << largest(a, b, c);
+
+    return 0;
+}
