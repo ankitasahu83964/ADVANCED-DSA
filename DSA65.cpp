8d675834b6b2135074a399d37f725181fb874013
Ankita Sahu <ankitasahu83964@gmail.com>
2026-09-24 20:52:21 +0530

    DSA65.cpp

---
+++ DSA65.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Function
+void greet() {
+    cout << "Hello, Welcome to C++!";
+}
+
+int main() {
+
+    // Function call
+    greet();
+
+    return 0;
+}
